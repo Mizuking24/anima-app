2b8cc7ac13481dcfcb131cc2ef66fd6f3fe34922
Mizuking24 <mi_sama.ww1224@icloud.com>
2021-09-07 15:34:55 +0000

    [Finish]コメント機能非同期通信

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,15 +1,15 @@
 class PostCommentsController < ApplicationController
   def create
-    anime = Anime.find_by(id: params[:anime_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.anime_id = anime.id
-    comment.save
-    redirect_to anime_path(anime.id)
+    @anime = Anime.find_by(id: params[:anime_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.anime_id = @anime.id
+    @comment.save
+    render :index
   end
 
   def destroy
-    PostComment.find_by(id: params[:id], anime_id: params[:anime_id]).destroy
-    redirect_to anime_path(params[:anime_id])
+    @comment = PostComment.find_by(id: params[:id], anime_id: params[:anime_id]).destroy
+    render :index
   end
 
   private
